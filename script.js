63ba9b1b9999d0b27157103cebcff1605c53a9f6
Jar'Zeno <jarzenojarrett@gmail.com>
2022-06-18 12:44:27 -0700

    Implement clear and delete functionality

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 // displayArray :
-// [Number, Number, String]
-// [storedValue, displayValue, operator]
+// [Str, Str, Str]
+// [storedValue, operator, displayValue]
 
-const btns = document.querySelectorAll('.button');
+
+const numBtns = document.querySelectorAll('.number');
+const arithmeticBtns = document.querySelectorAll('.arithmetic');
+const clearBtn = document.querySelector('.clear');
+const deleteBtn = document.querySelector('.delete');
+
 const display = document.querySelector('.display');
 
-let displayArray = [];
+let displayArray = ["", "", ""];
+let currentIndex = 0;
 
 // Add two number
 // IN > OUT: Number Number > Number
@@ -21,14 +27,14 @@ const mul = (x, y) => x * y;
 
 // Divides two Numbers
 // IN > OUT: Number Number > Number
-const div = (x, y) => x / y;
+const div = (x, y) => checkDenominator(y) ? "Nice Try" : x / y;
 
 // Check if denominator is 0;
 // IN > OUT: Number > Boolean 
 const checkDenominator = num => num == 0;
 
 // Operates on a Array consisting of two Numbers and a operator, returning the result
-// IN > OUT: [Number, Number, Operator] > Number
+// IN > OUT: displayArray > Number
 
 // Update display with the value of button pressed
 // Psuedeocode :
@@ -59,17 +65,82 @@ const checkDenominator = num => num == 0;
 // Else if Button is Delete
 
 function update() {
-    btns.forEach(btn => {
-        btn.addEventListener('click', () => {
-            if(!(isNaN(parseInt(btn.textContent))) || btn.textContent == '.') {
-                displayArray[0] += (btn.textContent);
-                display.textContent += `${btn.textContent}`;
-            } else {
-                displayArray.push(btn.textContent);
-                display.textContent = `${btn.textContent}`;
-            }
-        });
-    });
+    
 }
 
 update();
+
+
+// If Display has operator
+    // currentIndex + 1
+    // clear display
+    // Add Number to display
+    // Add Number to displayArray
+// Else
+    // Add Number to display
+    // Add Number to displayArray
+numBtns.forEach(btn => {
+    btn.addEventListener('click', () => {
+        if(isNaN(parseInt(display.textContent)) && display.textContent != "") {
+            currentIndex++;
+            displayArray[currentIndex] += btn.textContent;
+            display.textContent = `${btn.textContent}`;
+        } else {
+            displayArray[currentIndex] += btn.textContent;
+            display.textContent += `${btn.textContent}`;
+        }
+    });
+});
+
+// Add Arithmetic Operator to display and displayArray
+// If number in display
+    // currentIndex + 1
+    // clear display
+    // Add Number to display
+    // Add Number to displayArray
+// Else
+    // Add Number to display
+    // Add Number to displayArray
+arithmeticBtns.forEach(btn => {
+    btn.addEventListener('click', () => {
+        if(!isNaN(parseInt(display.textContent))) {
+            currentIndex++;
+            displayArray[currentIndex] += btn.textContent;
+            display.textContent = `${btn.textContent}`;
+        } else {
+            displayArray[currentIndex] += btn.textContent;
+            display.textContent += `${btn.textContent}`;
+        }
+    });
+});
+
+// Equals functionality
+
+// Decimal 
+// if btn int but not float
+
+// Clear / All Clear button
+// If display empty
+    // Clear all stored input
+// Else
+    // Clear display
+clearBtn.addEventListener('click', () => {
+    // delete all stored values in displayArray
+    if(display.textContent == "") {
+        currentIndex = 0;
+        for(let i = 0; i < displayArray.length; i++) {
+            displayArray[i] = "";
+        }
+    } else {
+        displayArray[currentIndex] = "";
+        display.textContent = "";
+    }
+});
+
+//Delete Button
+// If something in display
+    // Remove 1 character
+deleteBtn.addEventListener('click', () => {
+    display.textContent = display.textContent.substring(0, display.textContent.length-1);
+    displayArray[currentIndex] = display.textContent;
+});
