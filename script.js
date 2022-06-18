237ada63affb82dbfcc02d0acc0eade7075d5a6e
Jar'Zeno <jarzenojarrett@gmail.com>
2022-06-18 15:54:51 -0700

    Add rest of button functionality

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ const numBtns = document.querySelectorAll('.number');
 const arithmeticBtns = document.querySelectorAll('.arithmetic');
 const clearBtn = document.querySelector('.clear');
 const deleteBtn = document.querySelector('.delete');
+const equalsBtn = document.querySelector('.equals');
+const decimalBtns = document.querySelector('.decimal');
 
 const display = document.querySelector('.display');
 
@@ -35,41 +37,18 @@ const checkDenominator = num => num == 0;
 
 // Operates on a Array consisting of two Numbers and a operator, returning the result
 // IN > OUT: displayArray > Number
-
-// Update display with the value of button pressed
-// Psuedeocode :
-// Switch (Button)
-// If Button is Number
-    // If decimal in display
-        // Add display.value + number to display
-    // Else If NaN in display
-        // Clear display
-        // Add Number to display
-    // Else
-        // Add Number to display.value
-// Else if Button is Decimal
-    // If Number in display
-        // Add display.value + . to display
-    // Else
-        // Clear display
-        // Add 0. to display
-// Else if Button is Operator (+, -, *, /)
-    // If (* or /)
-        // Check if previous athritmetic operator
-            // If previouse operator (+ or -)
-                // do not calulate num1 (+ or -) num2
-                // add (* or /) to array
-                // then use order of operations to calulate total value
-// Else if Button is Equals
-// Else if Button is Clear
-// Else if Button is Delete
-
-function update() {
-    
+function operate(arr) {
+    switch(arr[1]) {
+        case '+':
+            return add(parseFloat(arr[0]), parseFloat(arr[2]));
+        case '-':
+            return sub(parseFloat(arr[0]), parseFloat(arr[2]));
+        case '*':
+            return mul(parseFloat(arr[0]), parseFloat(arr[2]));
+        case '/':
+            return div(parseFloat(arr[0]), parseFloat(arr[2]));
+    }
 }
-
-update();
-
 
 // If Display has operator
     // currentIndex + 1
@@ -103,21 +82,34 @@ numBtns.forEach(btn => {
     // Add Number to displayArray
 arithmeticBtns.forEach(btn => {
     btn.addEventListener('click', () => {
-        if(!isNaN(parseInt(display.textContent))) {
+        // DisplayArray Element 0 is empty
+        if (displayArray[0] == "") {
+            displayArray[currentIndex] = "";
+            display.textContent = "";
+        } 
+        // DisplayArray Element 3 contains number, operate on displayArray values
+        else if(displayArray[2] != "") {
+            let value = operate(displayArray);
+            currentIndex = 0;
+            for(let i = 0; i < displayArray.length; i++) {
+                displayArray[i] = "";
+            }
+            displayArray[0] = value.toString();
+            display.textContent = value.toString();
+        }
+        // Display is a number, add opperator
+        else if(!isNaN(parseInt(display.textContent))) {
             currentIndex++;
-            displayArray[currentIndex] += btn.textContent;
+            displayArray[currentIndex] = btn.textContent;
             display.textContent = `${btn.textContent}`;
-        } else {
-            displayArray[currentIndex] += btn.textContent;
-            display.textContent += `${btn.textContent}`;
+        }  
+        // Display is empty
+        else {
+            displayArray[currentIndex] = btn.textContent;
+            display.textContent = `${btn.textContent}`;
         }
     });
 });
-
-// Equals functionality
-
-// Decimal 
-// if btn int but not float
 
 // Clear / All Clear button
 // If display empty
@@ -144,3 +136,37 @@ deleteBtn.addEventListener('click', () => {
     display.textContent = display.textContent.substring(0, display.textContent.length-1);
     displayArray[currentIndex] = display.textContent;
 });
+
+// Equals
+// If displayArray[2] is not empty
+    // then call operate() on displayArray, store value in variable
+    // clear displayArray
+    // set displayArray[0] to value
+    // set display to value
+equalsBtn.addEventListener('click', () => {
+    if(displayArray[2] != "") {
+        let value = operate(displayArray);
+        currentIndex = 0;
+        for(let i = 0; i < displayArray.length; i++) {
+            displayArray[i] = "";
+        }
+        displayArray[0] = value.toString();
+        display.textContent = value.toString();
+    }
+});
+
+// Decimal 
+// if decimal is not in display and currentIndex not 1
+    // add decimal to display
+// else if display does not contain operator
+// else if display == ''
+    // add 0. to display
+decimalBtns.addEventListener('click', () => {
+    if(!display.textContent.includes('.') && currentIndex != 1) {
+        displayArray[currentIndex] += ".";
+        display.textContent += ".";
+    } else if(display.textContent == "") {
+        displayArray[currentIndex] = "0.";
+        display.textContent = "0.";
+    }
+})
